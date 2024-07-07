ec2e2aff9267d8cd90e7efcfb574c7865fe1e5bb
Luna Davis <ddavi142@asu.edu>
2024-07-07 12:16:57 -0700

    feat(Helpers): Expand with new utility functions

--- src/Helpers/Helpers.lua
+++ src/Helpers/Helpers.lua
@@ -3,3 +3,39 @@
   Author: ByteXenon [Luna Gilbert]
   Date: 2024-07-07
 --]]
+
+local Helpers = {}
+
+function Helpers.stringToTable(inputString)
+  local charArray, charArrayIndex = {}, 1
+  for char in inputString:gmatch(".") do
+    charArray[charArrayIndex] = char
+    charArrayIndex = charArrayIndex + 1
+  end
+  return charArray
+end
+
+function Helpers.convertToLookupTable(array)
+  local lookupTable = {}
+  for _, value in ipairs(array) do
+    lookupTable[value] = true
+  end
+  return lookupTable
+end
+
+function Helpers.convertToTrie(array)
+  -- Initialize the trie
+  local trie = {}
+  for _, op in ipairs(array) do
+    local node = trie
+    -- Split the operator into individual characters
+    for char in op:gmatch(".") do
+      node[char] = node[char] or {}
+      node = node[char]
+    end
+    node.Value = op
+  end
+  return trie
+end
+
+return Helpers
